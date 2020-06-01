a94b57d3cbe67ceb4a73c75cfa4404589fac2771
Keishaa <keishaavarma@gmail.com>
2020-06-01 20:02:49 +0530

    RED BOX

--- sketch.js
+++ sketch.js
@@ -15,10 +15,9 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 
-	fill("red");
-	box1=createSprite(400,660,130,10);
-	box2=createSprite(465,625,10,70);
-	box3=createSprite(335,625,10,70);
+	box1=createSprite(400,650,200,20);
+	box2=createSprite(500,600,20,100);
+	box3=createSprite(300,600,20,100);
 
 
 	packageSprite=createSprite(width/2, 80, 10, 10);
@@ -36,23 +35,30 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, packageBody);
+	var options= {
+		isStatic: true,
+		density:1,
+		friction:1
+	}
+
+	boxBody1= Bodies.rectangle(400,660,200,20,options);
+	 boxBody2= Bodies.rectangle(500,620,20,100,options);
+	 boxBody3= Bodies.rectangle(300,620,20,100,options);
+	 World.add(world,boxBody1);
+	 World.add(world,boxBody2);
+	 World.add(world,boxBody3);
+	 
+	 packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	 World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	 World.add(world, ground);
 	 
-	var options= {
-		isStatic: true
-	}
-	 boxBody1= Bodies.rectangle(400,660,130,10,options);
-	 boxBody2= Bodies.rectangle(465,625,10,70,options);
-	 boxBody3= Bodies.rectangle(335,625,10,70,options);
-	 World.add(world,boxBody1);
-	 World.add(world,boxBody2);
-	 World.add(world,boxBody3);
+	
+
+	
 
 
 	Engine.run(engine);
@@ -63,14 +69,18 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  box1.x= boxBody1.position.x
-  box1.y=boxBody1.position.y
-  box2.x= boxBody2.position.x
-  box2.y=boxBody2.position.y
-  box3.x= boxBody3.position.x
-  box3.y=boxBody3.position.y
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y ;
+  box1.shapeColor= "red";
+  box2.shapeColor= "red";
+  box3.shapeColor= "red";
+  box1.x= boxBody1.position.x;
+  box1.y=boxBody1.position.y;
+  box2.x= boxBody2.position.x;
+  box2.y=boxBody2.position.y;
+  box3.x= boxBody3.position.x;
+  box3.y=boxBody3.position.y;
   drawSprites();
  
 }
